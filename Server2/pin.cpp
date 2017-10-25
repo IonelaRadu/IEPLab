efa336b1cca51471b834a21e28c32887e82456f8
SchuplerSabrina <schupler.sabrina@gmail.com>
2017-10-25 16:26:15 +0300

    ok

--- Server2/pin.cpp
+++ Server2/pin.cpp
@@ -1,27 +1,38 @@
 #include "pin.h"
+#include "bcm2835.h"
 
-
-Pin::Pin(int var,int dir)
+Pin::Pin(int nr_p,int dir,int pull_up)
 {
-    this->p = var;
+    this->nr_p = nr_p;
     this->dir=dir;
+    this->pull_up=pull_up;
+    this->value = 0;
 }
-void Pin::get(int nr_p){
 
-
-
+char Pin::get(void)
+{
+    char stare;
+    if(this->dir==1){
+     stare = bcm2835_gpio_lev(this->nr_p);
+    }
+    else if(this->dir==0){
+     stare = bcm2835_gpio_lev(this->nr_p);
+    }
+    return stare;
 }
-void Pin::set(int nr_p,char pull_up){
-    if(nr_p output)
+void Pin::set(char value)
+{
+    // output=0
+    if(this->dir==0)
     {
-     bcm2835_gpio_write(nr_p, pull_up);
+        bcm2835_gpio_write(this->nr_p,value);
+        this->value = value;
     }
 }
-void Pin::change_dir(int nr_p){
+void Pin::change_dir(void){
 
-  if(bcm2835_gpio_lev(nr_p))
-
-       bcm2835_gpio_fsel(nr_p, BCM2835_GPIO_FSEL_OUTP);
+  if(bcm2835_gpio_lev(this->nr_p))
+       bcm2835_gpio_fsel(this->nr_p, BCM2835_GPIO_FSEL_OUTP);
   else
-      bcm2835_gpio_fsel(nr_p, BCM2835_GPIO_FSEL_INP);
+      bcm2835_gpio_fsel(this->nr_p, BCM2835_GPIO_FSEL_INPT);
 }
--- Server2/pin.h
+++ Server2/pin.h
@@ -4,8 +4,18 @@
 
 class Pin
 {
-public:
-    Pin();
-};
+    public:
+            Pin(int nr_p,int dir,int pull_up);
+    protected:
 
+           char get();
+           void set(char value);
+           void change_dir();
+           int nr_p;
+           int dir;
+           char pull_up;
+           char value;
+
+
+};
 #endif // PIN_H
